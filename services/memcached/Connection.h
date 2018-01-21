e20b5a30be6b8cd64a30833b931b02ea051cee94
nlfox <nlfox@msn.cn>
2018-01-21 12:19:21 +0000

    udp

--- services/memcached/Connection.h
+++ services/memcached/Connection.h
@@ -23,10 +23,15 @@
 #include "treadmill/StatisticsManager.h"
 #include "treadmill/Util.h"
 #include "treadmill/services/memcached/MemcachedService.h"
+#include <iostream>
+#include <boost/array.hpp>
+#include <boost/asio.hpp>
 
 DECLARE_string(hostname);
 DECLARE_int32(port);
 
+
+using boost::asio::ip::udp;
 using facebook::memcache::AsyncMcClient;
 using facebook::memcache::ConnectionOptions;
 using facebook::memcache::McDeleteRequest;
@@ -40,13 +45,53 @@ namespace facebook {
 namespace windtunnel {
 namespace treadmill {
 
+
+
+class UDPClient {
+public:
+    UDPClient(
+            boost::asio::io_service &io_service,
+            const std::string &host,
+            const std::string &port
+    ) : io_service_(io_service), socket_(io_service, udp::endpoint(udp::v4(), 0)) {
+        udp::resolver resolver(io_service_);
+        udp::resolver::query query(udp::v4(), host, port);
+        udp::resolver::iterator iter = resolver.resolve(query);
+        endpoint_ = *iter;
+    }
+
+    ~UDPClient() {
+        socket_.close();
+    }
+
+    void send(const std::string &msg) {
+        socket_.send_to(boost::asio::buffer(msg, msg.size()), endpoint_);
+        
+        size_t len = socket_.receive_from(
+                boost::asio::buffer(recv_buf), sender_endpoint);
+        //std::cout.write(recv_buf.data(), len);
+    }
+
+private:
+    boost::asio::io_service &io_service_;
+    udp::socket socket_;
+    boost::array<char, 1024> recv_buf;
+    udp::endpoint endpoint_;
+};
+
+
+
 template <>
 class Connection<MemcachedService> {
  public:
   explicit Connection<MemcachedService>(folly::EventBase& event_base) {
     std::string host = nsLookUp(FLAGS_hostname);
     ConnectionOptions opts(host, FLAGS_port, mc_ascii_protocol);
-    client_ = std::make_unique<AsyncMcClient>(event_base, opts);
+
+    
+    io_service_ = std::make_unique<boost::asio::io_service>();
+    client_ = std::make_unique<UDPClient>(io_service_, host, FLAGS_port);
+
     auto loopController = std::make_unique<EventBaseLoopController>();
     loopController->attachEventBase(event_base);
     fm_ = std::make_unique<FiberManager>(std::move(loopController));
@@ -56,26 +101,25 @@ class Connection<MemcachedService> {
 
   folly::Future<MemcachedService::Reply>
   sendRequest(std::unique_ptr<typename MemcachedService::Request> request) {
+
     folly::MoveWrapper<folly::Promise<MemcachedService::Reply> > p;
     auto f = p->getFuture();
 
     if (request->which() == MemcachedRequest::GET) {
-      auto req = std::make_shared<McGetRequest>(request->key());
-      fm_->addTask([this, req, p] () mutable {
-        client_->sendSync(*req, std::chrono::milliseconds::zero());
+      fm_->addTask([this, request, p] () mutable {
+        client_->send("get " + request->key()+"\r\n");
         p->setValue(MemcachedService::Reply());
       });
     } else if (request->which() == MemcachedRequest::SET) {
       auto req = std::make_shared<McSetRequest>(request->key());
       req->value() = folly::IOBuf(folly::IOBuf::COPY_BUFFER, request->value());
-      fm_->addTask([this, req, p] () mutable {
-        client_->sendSync(*req, std::chrono::milliseconds::zero());
+      fm_->addTask([this, request, p] () mutable {
+        client_->send("set" + " " + request->key() + " 0 900 " + std::to_string(request->value().length()) + "\r\n"+ request->value()+"\r\n");
         p->setValue(MemcachedService::Reply());
       });
     } else {
-      auto req = std::make_shared<McDeleteRequest>(request->key());
-      fm_->addTask([this, req, p] () mutable {
-        client_->sendSync(*req, std::chrono::milliseconds::zero());
+      fm_->addTask([this, request, p] () mutable {
+        client_->send("delete " + request->key()+"\r\n");
         p->setValue(MemcachedService::Reply());
       });
     }
@@ -84,6 +128,7 @@ class Connection<MemcachedService> {
 
  private:
   std::unique_ptr<AsyncMcClient> client_;
+  std::unique_ptr<boost::asio::io_service> io_service_;
   std::unique_ptr<FiberManager> fm_;
 };
 
