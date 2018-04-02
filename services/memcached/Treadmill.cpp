55c5f2ae9fcbce33f10e6363d8866277a5154a34
nlfox <nlfox@msn.cn>
2018-04-02 02:06:28 +0100

    mica

--- services/memcached/Treadmill.cpp
+++ services/memcached/Treadmill.cpp
@@ -9,7 +9,7 @@
  */
 
 #include "treadmill/services/memcached/Connection.h"
-#include "treadmill/services/memcached/Workload.h"
+#include "treadmill/services/memcached/WorkloadZipf.h"
 
 #include "treadmill/Treadmill.h"
 
